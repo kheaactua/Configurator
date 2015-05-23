a74575c82e2fc4ba0b946c0269274ac039c0788a
Matthew Russell <matthew.g.russell@gmail.com>
2015-05-23 11:20:33 -0400

    Reverted

---
+++ test/basic.php
@@ -0,0 +1,5 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
+
+$config = \Configurator\Constants::getRoot();
+print "Config found and loaded\n";
